ed0980e9cd1e1669d33c93fb3fefaf3fe7059d8a
kosombolt <kosomboltnew@gmail.com>
2025-06-28 06:31:53 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
   
 
 // https://vitejs.dev/config/
@@ -16,6 +12,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  
+  export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
   },
 });
