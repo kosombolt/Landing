ba1ab2e34ef87f4beae08c51091f7c4453425a3c
kosombolt <kosomboltnew@gmail.com>
2025-06-28 07:37:56 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 import { UserProvider } from '@/context/UserContext';
-import { BrowserRouter } from 'react-router-dom'; // ✅ import this
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ Wrap your whole app in Router */}
+    <BrowserRouter>
       <UserProvider>
         <App />
       </UserProvider>
