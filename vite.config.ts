cfa77df7a10d31fc19ac181d064a1b885f9a37de
kosombolt <kosomboltnew@gmail.com>
2025-06-28 06:31:20 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  
   },
 });
