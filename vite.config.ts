72724fb87e82d023cb7fcc1dad5e87726748ec5f
kosombolt <kosomboltnew@gmail.com>
2025-06-28 06:31:34 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
+  
 
 // https://vitejs.dev/config/
 export default defineConfig({
