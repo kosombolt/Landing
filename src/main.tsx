eebb464ce3b7472d69193eae689d31d948249d4d
kosombolt <kosomboltnew@gmail.com>
2025-06-28 06:29:23 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import React, { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [role, setRole] = useState(null);
+  return (
+    <UserContext.Provider value={{ role, setRole }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
